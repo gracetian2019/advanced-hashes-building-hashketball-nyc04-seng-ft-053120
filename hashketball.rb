e8dd816ee70d3d32b94972f089806b4e08ae5e3d
Grace Tian <gracetian4ever@gmail.com>
2020-06-15 02:36:12 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -57,24 +57,15 @@ def game_hash
         },
         {:player_name => "Jason Terry",
          :number => 31,
-         :shoe => 19,
-         :points => 26,
-         :rebounds => 11,
-         :assists => 6,
-         :steals => 3,
-         :blocks => 8,
-         :slam_dunks => 5
+         :shoe => 15,
+         :points => 19,
+         :rebounds => 2,
+         :assists => 2,
+         :steals => 4,
+         :blocks => 11,
+         :slam_dunks => 1
         }
-        ,,]},
-        # {:number => [0, 30, 11, 1, 31]},
-        # {:shoe => [16, 14, 17, 19, 15]},
-        # {:points => [22, 12, 17, 26, 19]},
-        # {:rebounds => [12, 12, 19, 11, 2]},
-        # {:assists => [12, 12, 10, 6, 2]},
-        # {:steals => [3, 12, 3, 3, 4]},
-        # {:blocks => [1, 12, 1, 8, 11]},
-        # {:slam_dunks => [1, 7, 15, 5, 1]}
-      ]
+       ]
     },
       
     :away => {
