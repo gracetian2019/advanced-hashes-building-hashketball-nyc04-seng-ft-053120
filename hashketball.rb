c08becd35c2f1617a72659ada33d79481649a4bd
Grace Tian <gracetian4ever@gmail.com>
2020-06-15 02:34:01 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,90 @@
 # Write your code here!
+def game_hash
+  game_hash = {
+    :home => {
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black", "White"],
+      :players => [
+        {:player_name => "Alan Anderson",
+         :number => 0,
+         :shoe => 16,
+         :points => 22,
+         :rebounds => 12,
+         :assists => 12,
+         :steals => 3,
+         :blocks => 1,
+         :slam_dunks => 1
+        },
+        {:player_name => "Reggie Evans",
+         :number => 30,
+         :shoe => 14,
+         :points => 12,
+         :rebounds => 12,
+         :assists => 12,
+         :steals => 12,
+         :blocks => 12,
+         :slam_dunks => 7
+        },
+        {:player_name => "Brook Lopez",
+         :number => 11,
+         :shoe => 17,
+         :points => 17,
+         :rebounds => 19,
+         :assists => 10,
+         :steals => 3,
+         :blocks => 1,
+         :slam_dunks => 15
+        },
+         {:player_name => "Mason Plumlee",
+         :number => 1,
+         :shoe => 19,
+         :points => 26,
+         :rebounds => 11,
+         :assists => 6,
+         :steals => 3,
+         :blocks => 8,
+         :slam_dunks => 5
+        },
+         {:player_name => "Mason Plumlee",
+         :number => 1,
+         :shoe => 19,
+         :points => 26,
+         :rebounds => 11,
+         :assists => 6,
+         :steals => 3,
+         :blocks => 8,
+         :slam_dunks => 5
+        }
+        ,,"Jason Terry"]},
+        # {:number => [0, 30, 11, 1, 31]},
+        # {:shoe => [16, 14, 17, 19, 15]},
+        # {:points => [22, 12, 17, 26, 19]},
+        # {:rebounds => [12, 12, 19, 11, 2]},
+        # {:assists => [12, 12, 10, 6, 2]},
+        # {:steals => [3, 12, 3, 3, 4]},
+        # {:blocks => [1, 12, 1, 8, 11]},
+        # {:slam_dunks => [1, 7, 15, 5, 1]}
+      ]
+    },
+      
+    :away => {
+      :team_name => "Charlotte Hornets",
+      :colors => ["Turquoise", "Purple"],
+      :players => [
+        {:player_name => ["Jeff Adrien","Bismack Biyombo","DeSagna Diop","Ben Gordon","Kemba Walker"]},
+        {:number => [4, 0, 2, 8, 33]},
+        {:shoe => [18, 16, 14, 15, 15]},
+        {:points => [10, 12, 24, 33, 6]},
+        {:rebounds => [1, 4, 12, 3, 12]},
+        {:assists => [1, 7, 12, 2, 12]},
+        {:steals => [2, 22, 4, 1, 7]},
+        {:blocks => [7, 15, 5, 1, 5]},
+        {:slam_dunks => [2, 10, 5, 0, 12]}
+      ]
+      
+    }
+    
+  }
+  
+  
+end
